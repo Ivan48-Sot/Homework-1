373c7f78ff10b625f73620d918e25e3ecc6278e0
Ivan48-Sot <iw4n.sot@yandex.ru>
2024-09-12 20:59:30 +0300

    Update lesson1.py

--- lesson1.py
+++ lesson1.py
@@ -1,7 +1,7 @@
 example = 'Сотников'
 print ( example [0] )
 print ( example [-1] )
-print ( example [4:7] )
-print ( example [::-1] )
 print ( example [2:7] )
+print ( example [::-1] )
+print ( example [2:5] )
 print ( example [1::2] )
